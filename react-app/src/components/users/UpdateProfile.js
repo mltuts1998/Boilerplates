c8a57a53573a76c03da262f2e8c21bd63fa30b25
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-06-16 16:52:16 +0530

    Added User Update component

--- react-app/src/components/users/UpdateProfile.js
+++ react-app/src/components/users/UpdateProfile.js
@@ -9,6 +9,13 @@ import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -27,20 +34,38 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
+const formatDate = (date) => {
+    let processed_date = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`
+    return processed_date;
+};
+
+
 const Login = () => {
     const classes = useStyles();
     const { name, changeName, deleteName } = useContext(GlobalContext);
     const [values, setValues] = React.useState({
-        email: '',
+        username: '',
+        firstname: '',
+        lastname: '',
         password: '',
+        email: '',
+        confirm_password: '',
+        date_of_birth: new Date()
       });
+    
 
     const handleChange = (prop) => (event) => {
         setValues({ ...values, [prop]: event.target.value });
     };
 
+    
+    const handleDateChange = (date) => {
+        setValues({ ...values, date_of_birth: date });
+    };
+
     const submitForm = (event) => {
         // Make Request Here
+        console.log(values);
     }
     
     return (    
@@ -53,7 +78,7 @@ const Login = () => {
             >
                 
                 <Typography variant="h4" gutterBottom style={{textAlign: "center"}}>
-                    Login
+                    Update Profile
                 </Typography>
 
                 <Divider />
@@ -61,6 +86,50 @@ const Login = () => {
                 <div
                     style={{width: "100%", padding: "20px"}}
                 >
+                    <FormControl  fullWidth variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-email">Username</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-username"
+                        value={values.username}
+                        onChange={handleChange('username')}
+                        startAdornment={<InputAdornment position="start"></InputAdornment>}
+                        labelWidth={60}
+                        type="text"
+                        disabled="true"
+                    />
+                    </FormControl>
+
+                    <div style={{padding: "10px"}} />
+
+                    <FormControl  fullWidth variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-firstname">First Name</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-firstname"
+                        value={values.firstname}
+                        onChange={handleChange('firstname')}
+                        startAdornment={<InputAdornment position="start"></InputAdornment>}
+                        labelWidth={60}
+                        type="text"
+                    />
+                    </FormControl>
+
+                    <div style={{padding: "10px"}} />
+
+
+                    <FormControl  fullWidth variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-email">Last Name</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-lastname"
+                        value={values.lastname}
+                        onChange={handleChange('lastname')}
+                        startAdornment={<InputAdornment position="start"></InputAdornment>}
+                        labelWidth={60}
+                        type="text"
+                    />
+                    </FormControl>
+
+                    <div style={{padding: "10px"}} />
+
                     <FormControl  fullWidth variant="outlined">
                     <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
                     <OutlinedInput
@@ -70,10 +139,11 @@ const Login = () => {
                         startAdornment={<InputAdornment position="start"></InputAdornment>}
                         labelWidth={60}
                         type="email"
+                        disabled="true"
                     />
                     </FormControl>
 
-                    <div style={{padding: "20px"}} />
+                    <div style={{padding: "10px"}} />
 
                     <FormControl  fullWidth variant="outlined">
                     <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
@@ -86,6 +156,42 @@ const Login = () => {
                         type="password"
                     />
                     </FormControl>
+
+                    <div style={{padding: "10px"}} />
+
+                    <FormControl  fullWidth variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-confirm-password">Confirm Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-confirm-password"
+                        value={values.confirm_password}
+                        onChange={handleChange('confirm_password')}
+                        startAdornment={<InputAdornment position="start"></InputAdornment>}
+                        labelWidth={60}
+                        type="password"
+                    />
+                    </FormControl>
+
+                    <div style={{padding: "10px"}} />
+                    
+
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                            margin="normal"
+                            id="date-picker-dialog"
+                            label="Date Of Birth"
+                            format="MM/dd/yyyy"
+                            value={values.date_of_birth}
+                            onChange={handleDateChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                            style={{
+                                width: "100%"
+                            }}
+                        />
+                    </MuiPickersUtilsProvider>
+
+
                 </div>
                 
                 <div
@@ -100,8 +206,9 @@ const Login = () => {
                         }}
 
                         onClick={ () => submitForm() } 
+                        disabled={ values.password != values.confirm_password }
                     >
-                        Login
+                        Update
                     </Button>
                 </div>
 
