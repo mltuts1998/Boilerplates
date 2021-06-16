26f01e2d885262a8a248d187d5bc8d128a649d81
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-06-16 12:45:48 +0530

    Added React app

---
+++ react-app/src/views/test/Test.js
@@ -0,0 +1,15 @@
+import React, { userState, useEffect, useRef } from 'react';
+import Drawer from '../../components/Drawer.js';
+
+const Test = () => {
+    return (
+        <div>
+            <Drawer>
+                {/* All components */}
+            </Drawer>
+        </div>
+    )
+}
+
+
+export default Test;
